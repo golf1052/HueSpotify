7d08a71ad278466c92174a6b841ba9ec068f8b4e
Sanders Lauture <golf1052@msn.com>
2017-06-16 20:02:54 -0700

    Add preliminary music code

---
+++ HueSpotify/AdjustableMax.cs
@@ -0,0 +1,47 @@
+﻿namespace HueSpotify
+{
+    public class AdjustableMax
+    {
+        public float CurrentMax { get; private set; }
+        private float lastValue;
+        private float value;
+        public float Value
+        {
+            get
+            {
+                if (CurrentMax == 0)
+                {
+                    return 0;
+                }
+                float returningValue = value / CurrentMax;
+                return returningValue;
+            }
+            set
+            {
+                this.lastValue = this.value;
+                this.value = value;
+                if (CurrentMax < value)
+                {
+                    CurrentMax = value;
+                }
+            }
+        }
+
+        public AdjustableMax()
+        {
+            CurrentMax = 0;
+            value = 0;
+        }
+
+        public void Reset()
+        {
+            Reset(0);
+        }
+
+        public void Reset(float percent)
+        {
+            CurrentMax *= percent;
+            value = 0;
+        }
+    }
+}
--- HueSpotify/MainPage.xaml.cs
+++ HueSpotify/MainPage.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Audio;
+using Windows.Media.Render;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +27,104 @@ namespace HueSpotify
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        AudioGraph audioGraph;
+        AudioFrameOutputNode frameOutputNode;
+
+        DeviceInformation audioInput;
+        DeviceInformation audioOutput;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var audioInputDevices = await DeviceInformation.FindAllAsync(DeviceClass.AudioCapture);
+            foreach (var device in audioInputDevices)
+            {
+                if (device.Name.Contains("Output"))
+                {
+                    audioInput = device;
+                }
+            }
+            var audioOutputDevices = await DeviceInformation.FindAllAsync(DeviceClass.AudioRender);
+            foreach (var device in audioOutputDevices)
+            {
+                if (device.Name.Contains("Input"))
+                {
+                    audioOutput = device;
+                }
+            }
+
+            AudioGraphSettings audioGraphSettings = new AudioGraphSettings(AudioRenderCategory.Media);
+            audioGraphSettings.DesiredSamplesPerQuantum = 440;
+            audioGraphSettings.DesiredRenderDeviceAudioProcessing = AudioProcessing.Default;
+            audioGraphSettings.QuantumSizeSelectionMode = QuantumSizeSelectionMode.ClosestToDesired;
+            audioGraphSettings.PrimaryRenderDevice = audioOutput;
+            CreateAudioGraphResult audioGraphResult = await AudioGraph.CreateAsync(audioGraphSettings);
+            if (audioGraphResult.Status != AudioGraphCreationStatus.Success)
+            {
+                Debug.WriteLine("AudioGraph creation failed! " + audioGraphResult.Status);
+                return;
+            }
+            audioGraph = audioGraphResult.Graph;
+            CreateAudioDeviceInputNodeResult inputNodeResult = await audioGraph.CreateDeviceInputNodeAsync(Windows.Media.Capture.MediaCategory.Media, audioGraph.EncodingProperties, audioInput);
+            if (inputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                Debug.WriteLine("AudioDeviceInputNode creation failed! " + inputNodeResult.Status);
+                return;
+            }
+            AudioDeviceInputNode inputNode = inputNodeResult.DeviceInputNode;
+            CreateAudioDeviceOutputNodeResult outputNodeResult = await audioGraph.CreateDeviceOutputNodeAsync();
+            if (outputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                Debug.WriteLine("AudioDeviceOutputNode creation failed! " + outputNodeResult.Status);
+                return;
+            }
+            AudioDeviceOutputNode outputNode = outputNodeResult.DeviceOutputNode;
+            frameOutputNode = audioGraph.CreateFrameOutputNode();
+            inputNode.AddOutgoingConnection(frameOutputNode);
+            //inputNode.AddOutgoingConnection(outputNode);
+
+            //for (int i = 0; i < 220; i++)
+            //{
+            //    maxListLeft.Add(new AdjustableMax());
+            //    maxListRight.Add(new AdjustableMax());
+            //}
+
+            audioGraph.QuantumProcessed += AudioGraph_QuantumProcessed;
+            audioGraph.UnrecoverableErrorOccurred += AudioGraph_UnrecoverableErrorOccurred;
+            audioGraph.Start();
+            inputNode.Start();
+            //outputNode.Start();
+            frameOutputNode.Start();
+        }
+
+        private void AudioGraph_QuantumProcessed(AudioGraph sender, object args)
+        {
+            AudioFrame audioFrame = frameOutputNode.GetFrame();
+            List<float[]> amplitudeData = HelperMethods.ProcessFrameOutput(audioFrame);
+            List<float[]> channelData = HelperMethods.GetFftData(HelperMethods.ConvertTo512(amplitudeData, audioGraph), audioGraph);
+            for (int i = 0; i < channelData.Count / 2; i++)
+            {
+                float[] leftChannel = channelData[i];
+                float[] rightChannel = channelData[i + 1];
+
+                for (int j = 0; j < 220; j++)
+                {
+                    //maxListLeft[j].Value = leftChannel[j];
+                    //maxListRight[j].Value = rightChannel[j];
+                }
+
+                //averageLowLeft.Value = HelperMethods.Average(leftChannel, 0, 16);
+                //averageLowRight.Value = HelperMethods.Average(rightChannel, 0, 16);
+            }
+        }
+
+        private void AudioGraph_UnrecoverableErrorOccurred(AudioGraph sender, AudioGraphUnrecoverableErrorOccurredEventArgs args)
+        {
+            Debug.WriteLine("UNRECOVERABLE ERRORRRRRR");
         }
     }
 }
